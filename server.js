99c1fa26fed7c1ebc6057a28f159624e9f0e7367
Superior5 <superiorunknown5@gmail.com>
2023-01-06 22:25:53 +0300

    Развертывание проекта

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.listen(5000, ()=> {
+    console.log('server work');
+});
